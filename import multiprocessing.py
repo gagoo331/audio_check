e1b0185b34af1b5b27d413cdda5a8835e54cd3c2
gagoo331 <35999010+gagoo331@users.noreply.github.com>
2022-01-11 12:26:53 -0800

    Delete import multiprocessing.py

--- import multiprocessing.py
+++
@@ -1,7 +0,0 @@
-import multiprocessing
-from playsound import playsound
-
-p = multiprocessing.Process(target=playsound, args=("C:\\Users\\gagoo\\Documents\\Sound\\file_example_WAV_2MG.wav",))
-p.start()
-input("press ENTER to stop playback")
-p.terminate()
